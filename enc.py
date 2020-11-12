0e767c3aee9ecce1893fbef16e54eab77907a44a
Artur <funcelot@outlook.com>
2020-11-12 12:16:08 +0300

    default encoder

--- enc.py
+++ enc.py
@@ -1,4 +1,5 @@
 #import dis
+import time
 import hashlib
 import math
 from operator import itemgetter
@@ -32,10 +33,17 @@ def chars(text):
 IV = 1
 shift = 1
 
-alphabet = """QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890 .
+alphabet = """~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./ 
 """
-plaintext = "A"
 sha_alphabet = hashlib.sha1(alphabet.encode("utf-8")).hexdigest()
+plaintext = """The atmosphere of Mars is about 100 times thinner than Earth's, and it is 95 percent carbon dioxide. Here's a breakdown of its composition, according to a NASA fact sheet:
+
+Carbon dioxide: 95.32 percent
+Nitrogen: 2.7 percent
+Argon: 1.6 percent
+Oxygen: 0.13 percent
+Carbon monoxide: 0.08 percent
+Also, minor amounts of: water, nitrogen oxide, neon, hydrogen-deuterium-oxygen, krypton and xenon."""
 sha_plaintext = hashlib.sha1(plaintext.encode("utf-8")).hexdigest()
 
 class prng:
@@ -57,14 +65,17 @@ class prng:
       b = argv[1]
       return (self._seed / 2147483647) * (b - a) + a
 
-max = 2147483647
 min = 0
-
-def random():
-  return math.floor(rnd.next(min, max))
+max = 2147483647
 
 seed = 1238473661
+# seed = time.time()
+
 rnd = prng(seed)
+
+def random():
+  global rnd
+  return math.floor(rnd.next(min, max))
 
 def size():
   return len(alphabet)
@@ -74,11 +85,17 @@ def next_position(alphabet, char):
   position = alphabet.index(char)
   return (position + 1 + j) % size()
 
+def previous_position(alphabet, char):
+  j = random()
+  position = alphabet.index(char)
+  return size() - 1 - ((size() - position + j) % size())
+
 def hex2binb(string):
   result = []
   while len(string) >= 8:
     result.append(int(string[:8], 16))
     string = string[8:]
+  #return [i ^ 0xFFFFFFFF for i in result]
   return result
 
 def shuffle(array, seed):
@@ -88,34 +105,68 @@ def shuffle(array, seed):
     array[i], array[j] = array[j], array[i]
 
 def shuffle_binb(alphabet, str):
-  array = hex2binb(str)
+  if isinstance(str, bytes):
+    array = str
+  else:
+    array = hex2binb(str)
   shuffle(alphabet, array[0])
   shuffle(alphabet, array[1])
   shuffle(alphabet, array[2])
   shuffle(alphabet, array[3])
+  shuffle(alphabet, array[4])
 
 def cipher_function(cipher):
-  def function(random, shift, alpha, array, sha_alphabet, sha_plaintext):
+  def function(random, shift, alphabet, array, sha_alphabet, sha_plaintext):
     global rnd
-    alphabet = alpha
     shuffle_binb(alphabet, sha_alphabet)
     shuffle_binb(alphabet, sha_plaintext)
     rnd = prng(random)
-    for i in range(shift):
-      array = map(cipher, array)
+    i = 0
+    while i < shift:
+      array = list(map(cipher(alphabet), array))
+      i = i + 1
     return list(array)
   return function
 
-def shift_encrypt(char):
-  position = alphabet.index(char)
-  newPosition = next_position(alphabet, char)
-  while newPosition == position:
+def shift_encrypt(alphabet):
+  def enc(char):
+    if char == '' or (not char in alphabet): raise Exception("undefined char '" + char + "'")
+    position = alphabet.index(char)
     newPosition = next_position(alphabet, char)
-  return alphabet[newPosition]
+    while newPosition == position:
+      newPosition = next_position(alphabet, char)
+    return alphabet[newPosition]
+  return enc
+
+def shift_decrypt(alphabet):
+  def enc(char):
+    if char == '' or (not char in alphabet): raise Exception("undefined char '" + char + "'")
+    position = alphabet.index(char)
+    newPosition = previous_position(alphabet, char)
+    while newPosition == position:
+      newPosition = previous_position(alphabet, char)
+    return alphabet[newPosition]
+  return enc
 
 def encrypt_cipher(IV, shift, alphabet, plaintext, sha_alphabet, sha_plaintext):
-  return cipher_function(shift_encrypt)(IV, shift, [*alphabet], [*plaintext], sha_alphabet, sha_plaintext)
+  return cipher_function(shift_encrypt)(IV, shift, alphabet, plaintext, sha_alphabet, sha_plaintext)
 
-encoded = encrypt_cipher(IV, shift, alphabet, plaintext, sha_alphabet, sha_plaintext)
+def decrypt_cipher(IV, shift, alphabet, plaintext, sha_alphabet, sha_plaintext):
+  return cipher_function(shift_decrypt)(IV, shift, alphabet, plaintext, sha_alphabet, sha_plaintext)
 
+random_alphabet = [*alphabet]
+
+import random as PRNG
+#shuffle(random_alphabet, math.floor(rng * max))
+
+alphabet = "".join(random_alphabet)
+
+
+#alphabet = [chr(i) for i in range(0,0x10ffff)]
+#PRNG.shuffle(alphabet)
+
+encoded = encrypt_cipher(IV, shift, [*alphabet], [*plaintext], sha_alphabet, sha_plaintext)
 print(encoded)
+
+decoded = decrypt_cipher(IV, shift, [*alphabet], [*encoded], sha_alphabet, sha_plaintext)
+print(decoded)
